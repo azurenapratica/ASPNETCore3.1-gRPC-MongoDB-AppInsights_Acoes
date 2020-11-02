a0012a19287eb8cc30eca9d9a58f12f541c38b13
Renato Groffe <renato.groffe@yahoo.com.br>
2020-11-02 18:19:01 -0300

    ASP.NET Core 3.1 + gRPC + MongoDB + FluentValidation + Application Insighs + Dockerfile + Cotações de Ações
    ASP.NET Core 3.1 + gRPC + MongoDB + FluentValidation + Application Insighs + Dockerfile + Cotações de Ações

---
+++ gRPCAcoes/Startup.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using gRPCAcoes.Data;
+
+namespace gRPCAcoes
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddScoped<AcoesRepository>();
+            services.AddGrpc();
+            services.AddApplicationInsightsTelemetry(Configuration);
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapGrpcService<ControladorAcoesService>();
+
+                endpoints.MapGet("/", async context =>
+                {
+                    await context.Response.WriteAsync("Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
+                });
+            });
+        }
+    }
+}
